f98b511344fe1c73510ae1b78407b04d1c6c9c49
Yami-no-karuro <carloborgna98@gmail.com>
2025-04-15 09:40:55 +0200

    HashTable

---
+++ src/lib/hash_table.h
@@ -0,0 +1,29 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#define FNV_OFFSET_BASIS 14695981039346656037U
+#define FNV_PRIME 1099511628211U
+
+typedef struct HashNode {
+    char *key;
+    int value;
+    struct HashNode *next;
+} HashNode;
+
+typedef struct {
+    HashNode **buckets;
+    int size;
+    int count;
+} HashTable;
+
+HashTable *ht_create(int size);
+
+void ht_resize(HashTable *table, int new_size);
+void ht_insert(HashTable *table, const char *key, int value);
+void ht_delete(HashTable *table, const char *key);
+void ht_free(HashTable *table);
+int ht_search(HashTable *table, const char *key);
+
+unsigned long ht_hash(const char *str, unsigned int table_size);
+
+#endif
