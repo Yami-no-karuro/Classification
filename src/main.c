1368ac9311ec506b8e297dce562e2cabbb4d8710
Yami-no-karuro <carloborgna98@gmail.com>
2025-04-15 22:38:16 +0200

    Refactoring

--- src/main.c
+++ src/main.c
@@ -20,10 +20,10 @@ int main()
     cls_train(classifier, "Lunch at 1pm sounds great, see you then.", CLASS_HAM);
 
     const char *input = "Can we have a meeting tomorrow to discuss the report?";
-    MailClass result1 = cls_predict(classifier, input);
+    MailClass prediction = cls_predict(classifier, input);
 
     printf("Input: %s\n", input);
-    printf("Prediction: %s\n", result1 == CLASS_SPAM ? "SPAM" : "HAM");
+    printf("Prediction: %s\n", prediction == CLASS_SPAM ? "SPAM" : "HAM");
 
     cls_free(classifier);
     return 0;
